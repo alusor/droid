59e83bc876a13335baaae0f9d688df04a58ccfc4
Alusor <eddvelez@outlook.com>
2015-10-08 02:53:55 -0500

    Xamarin test

---
+++ droid/droid/divisaspage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace droid
+{
+    public partial class divisaspage : ContentPage
+    {
+        public divisaspage()
+        {
+            InitializeComponent();
+        }
+    }
+}
